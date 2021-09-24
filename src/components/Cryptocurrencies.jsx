84b1cc7ce0caefb1d440f4fea8839934e3b3573d
Nardin Lachowski <nardin@live.com>
2021-09-24 13:38:15 -0400

    created component file with standard react component export

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cryptocurrencies = () => {
+    return (
+        <div>
+            <p>Cryptocurrencies</p>
+        </div>
+    )
+}
+
+export default cryptocurrencies

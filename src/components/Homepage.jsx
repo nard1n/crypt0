c153541c8707d80fe1ddaae06c63c2f8d45c34d0
Nardin Lachowski <nardin@live.com>
2021-09-27 19:55:37 -0400

    importing the news and cryptocurrencies in and redenring the components along with section titles and link to see more info

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 import { useGetCryptosQuery } from '../services/cryptoApi'; 
 
@@ -24,6 +26,16 @@ const Homepage = () => {
             <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
             <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
         </Row>
+        <div className="home-heading-container">
+            <Title level={2} className="home-title">Top 10 Cryptocurrencies in the World</Title>
+            <Title level={3} className="show-more"><Link to="/currencies">Show More</Link></Title>
+        </div>
+        <Cryptocurrencies simplified />
+        <div className="home-heading-container">
+            <Title level={2} className="home-title">Latest Crypto News</Title>
+            <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+        </div>
+        <News simplified />
         </>
     )
 }

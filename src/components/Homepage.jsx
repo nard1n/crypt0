373d91a602c761d2c4f00dbbeb2ba0b1688eccf4
Nardin Lachowski <nardin@live.com>
2021-09-28 09:07:30 -0400

    limiting cryptos on homepage to 10 and corrected show more cryptos link url

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -10,7 +10,7 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 const { Title } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
     
     console.log(data);
@@ -28,7 +28,7 @@ const Homepage = () => {
         </Row>
         <div className="home-heading-container">
             <Title level={2} className="home-title">Top 10 Cryptocurrencies in the World</Title>
-            <Title level={3} className="show-more"><Link to="/currencies">Show More</Link></Title>
+            <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
         </div>
         <Cryptocurrencies simplified />
         <div className="home-heading-container">

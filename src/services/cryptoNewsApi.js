83d395ba3238d21ea3bb3d2bf313e5ac826fcb8b
Nardin Lachowski <nardin@live.com>
2021-09-28 19:48:27 -0400

    built the api to call on bing news api and get articles for our news section, will be moving the endpoint strings to .env file later with new ones for security measures

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'f12b730b50msh4aa304f79102f3bp1300aajsn810a83167c1c'
+  }
+
+  const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        }),       
+    }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;

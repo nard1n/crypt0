fdae58961cc3c742ec12ba27328291061209ada4
Nardin Lachowski <nardin@live.com>
2021-09-29 08:50:05 -0400

    began to build the coin details page that will be presented when user clicks on any of the listed coins to gt more info. The page has not been built out yet but the data is being rendered into the console from the api

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import millify from 'millify';
+import { useParams } from 'react-router-dom';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
+    const { coinId } = useParams();
+    const [timePeriod, setTimePeriod] = useState('7d');
+    const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+
+    console.log("coinData", data);
+
     return (
         <div>
             <h1>CryptoDetails</h1>

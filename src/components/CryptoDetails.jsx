eb3f0d8b36548ccbbd64c82e68745ee96d2169bb
Nardin Lachowski <nardin@live.com>
2021-10-07 16:49:23 -0400

    importing new api endpoint call, added fetching data message to prevent fetching error and importing the new linechart component

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -4,8 +4,8 @@ import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
-
-import { useGetCryptoDetailsQuery  } from '../services/cryptoApi';
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery  } from '../services/cryptoApi';
+import LineChart from './LineChart';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -14,7 +14,10 @@ const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timePeriod, setTimePeriod] = useState('7d');
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timePeriod});
   const cryptoDetails = data?.data?.coin;
+
+  if(isFetching) return 'Loading...';
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
@@ -52,7 +55,7 @@ const CryptoDetails = () => {
             {time.map((date) => <Option key={date}>{date}</Option>)}
       </Select>
       
-      {/* Linechart */}
+      <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} />
       
       <Col className="stats-container">
         <Col className="coin-value-statistics">

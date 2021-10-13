6a8300bfd1cb44e1ce5d582c0d70dd781a8826ea
Nardin Lachowski <nardin@live.com>
2021-10-13 09:42:54 -0400

    corrected typo in exchanges path and minor formatting edit

--- src/App.js
+++ src/App.js
@@ -5,51 +5,49 @@ import { Layout, Typography, Space } from 'antd';
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 import './App.css';
 
-const App = () => {
-    return (
-        <div className="app">
-            <div className="navbar">
-                <Navbar />
-            </div>
-            <div className="main">
-                <Layout>
-                    <div className="routes">
-                        <Switch>
-                            <Route exact path="/">
-                                <Homepage />
-                            </Route>
-                        </Switch>
-                        <Switch>
-                            <Route exact path="/extchanges">
-                                <Exchanges />
-                            </Route>
-                            <Route exact path="/cryptocurrencies">
-                                <Cryptocurrencies />
-                            </Route>
-                            <Route exact path="/crypto/:coinId">
-                                <CryptoDetails />
-                            </Route>
-                            <Route exact path="/news">
-                                <News />
-                            </Route>
-                        </Switch>
-                    </div>
-                </Layout>
-            
-                <div className="footer" level={5}>
-                    <Typography.Title style={{ color: 'white', textAlign: 'center' }}>
-                        Cryptostat <br />
-                        All rights reserved
-                    </Typography.Title>
-                    <Space>
-                        <Link to="/">Home</Link>
-                        <Link to="/exchanges">Exchanges</Link>
-                        <Link to="/news">News</Link>
-                    </Space>
+const App = () => (
+    <div className="app">
+        <div className="navbar">
+            <Navbar />
+        </div>
+        <div className="main">
+            <Layout>
+                <div className="routes">
+                    <Switch>
+                        <Route exact path="/">
+                            <Homepage />
+                        </Route>
+                    </Switch>
+                    <Switch>
+                        <Route exact path="/exchanges">
+                            <Exchanges />
+                        </Route>
+                        <Route exact path="/cryptocurrencies">
+                            <Cryptocurrencies />
+                        </Route>
+                        <Route exact path="/crypto/:coinId">
+                            <CryptoDetails />
+                        </Route>
+                        <Route exact path="/news">
+                            <News />
+                        </Route>
+                    </Switch>
                 </div>
+            </Layout>
+        
+            <div className="footer" level={5}>
+                <Typography.Title style={{ color: 'white', textAlign: 'center' }}>
+                    Cryptostat <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
             </div>
         </div>
-    )
-}
+    </div>
+);
 
 export default App;
